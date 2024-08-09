bb3d4dabe92ab8b412cb0ad45a017a23f7d42a8b
Xunil <48892193+XunilKoburax@users.noreply.github.com>
2024-08-09 14:02:40 -0600

    Se agrega primer componente basado en el encabezado

---
+++ src/encabezado.jsx
@@ -0,0 +1,30 @@
+export default function Header(){
+    return(
+        <header>
+        <nav>
+          <h1>Coffie code</h1>
+        <ul id="barnav" class="nav">
+            <li><a href='#'>Tienda en linea</a></li>
+            <li>|</li>
+            <li><a href="#">Código ejemplo</a></li>
+            <li>|</li>
+            <li><a href="#">Plantillas</a></li>
+            <li>|</li>
+            <li class="cart-container">
+              <a href="#"><img src=".\\public\\car_white.png" alt="" height="30px" /></a>
+              <div class="cart-dropdown">
+                <p>Carrito vacío</p>
+                <div className="deailtcard">
+                  <div className="name">Bulbasaur</div>
+                  <div className="points">50 pts</div>
+                  <div className="count">1</div>
+                  <div className="total">50 pts</div>
+                </div>
+              </div>
+            </li>
+           
+        </ul>
+        </nav>
+    </header>
+    )
+}

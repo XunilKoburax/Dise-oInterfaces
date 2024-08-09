e6724e41c282bdebccc98e76f54300ea32a1c7a0
Your Name <you@example.com>
2024-08-09 12:58:44 -0600

    Agregar encabezado

--- src/App.jsx
+++ src/App.jsx
@@ -32,74 +32,7 @@ function App() {
         </ul>
         </nav>
     </header>
-    <div class="Gallery"> 
-    <card>
-      <div class="main-container">
-        <div className="img-container">
-        <img src="public\\pkm\\1891758-001bulbasaur.png" alt="" />
-        </div>
-        <div className="content">
-          <div className="title">
-            <h2>Bulbasaur</h2>
-          </div>
-          <div className="description">
-            <p>Pokemon tipo Hierba y veneno</p>
-          </div>
-          <div className="price">
-            <h3>50 pts</h3>
-          </div>
-          <div className="add">
-            <button>Add</button>
-          </div>
-        </div>
-
-      </div>
-    </card>
-    <card>
-      <div class="main-container">
-        <div className="img-container">
-        <img src="public\\pkm\\1891759-002ivysaur.png" alt="" />
-        </div>
-        <div className="content">
-          <div className="title">
-            <h2>Ivysaur</h2>
-          </div>
-          <div className="description">
-            <p>Pokemon tipo Hierba y veneno</p>
-          </div>
-          <div className="price">
-            <h3>60 pts</h3>
-          </div>
-          <div className="add">
-            <button>Add</button>
-          </div>
-        </div>
-
-      </div>
-    </card>
-    <card>
-      <div class="main-container">
-        <div className="img-container">
-        <img src="public\\pkm\\1891760-003venusaur.png" alt="" />
-        </div>
-        <div className="content">
-          <div className="title">
-            <h2>Venusaur</h2>
-          </div>
-          <div className="description">
-            <p>Pokemon tipo Hierba y veneno</p>
-          </div>
-          <div className="price">
-            <h3>70 pts</h3>
-          </div>
-          <div className="add">
-            <button>Add</button>
-          </div>
-        </div>
-
-      </div>
-    </card>
-    </div> 
+    
     </>
   )
 }

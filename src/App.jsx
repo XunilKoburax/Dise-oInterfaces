fd559965c703022d51a45954417d035c6ffcf7ba
baddeveloper <elmalprogramador@example.com>
2024-08-12 22:13:07 -0600

    Se agregarn funciones tipo useState y funciones useEffect para renderizar la vista segun un documento externo que servira como base de datos

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 import Header from './encabezado'
 import Tarjeta from './tarjetas'
-
+import {db} from './data/pkm.js'
 import './App.css'
 
+
+
 function App() {
+  //declaracion de estados
+  const [pokemons, setPokemon] = useState([]) // los hooks se debe de colocar
+  // siempre antes del return para que se tenga control de lo que se esta declarando
+  const [total, setTotal] = useState(0)
+  // de igual modo nunca se debe declarar dentro de condicionales pues puede generar problemas
+  const [carrito, setCarrito] = useState([])
+  useEffect(()=>{
+    setPokemon(db)
+    console.log("setEffect")
+  }, []
+)
 
   return (
     <>
      <Header></Header>
     <div className="Gallery"> 
-        <Tarjeta></Tarjeta>
+        {pokemons.map (() =>(
+          <Tarjeta />
+        ))}
+        
+        
     </div> 
     </>
   )
---
+++ src/data/pkm.js
@@ -0,0 +1,20 @@
+export const db = [
+   {"name":"Bulbasaur",
+    "id":1,
+    "type":["Veneno","Planta"],
+    "Costo":"50 pts",
+    "img":"\\pkm\\1891758-001bulbasaur.png"
+    },
+    {"name":"Ivysaur",
+    "id":1,
+    "type":["Veneno","Planta"],
+    "Costo":"50 pts",
+    "img":"\\pkm\\1891759-002ivysaur.png"
+    },
+    {"name":"Venusaur",
+    "id":1,
+    "type":["Veneno","Planta"],
+    "Costo":"50 pts",
+    "img":"\\pkm\\1891760-003venusaur.png"
+    }
+]
